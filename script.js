a9c0988b8104e6e676fe61848b1337f6187dd845
bradlloy95 <136913328+bradlloy95@users.noreply.github.com>
2024-03-24 15:02:51 +0000

    added winning combinations and outputs

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
 function getComputerChoice() {
-    let choices = ["Rock",
-    "Paper",
-    "Scissors"]
+    let choices = ["rock",
+    "paper",
+    "scissors"]
 
     let index = Math.floor(Math.random() * 3);
     return choices[index]
 
 }
 
-console.log(getComputerChoice())
+const computerSelection = getComputerChoice();
+const playersSelection = "rock";
+console.log(computerSelection)
+
+if (computerSelection === playersSelection) {
+    console.log("Draw!")
+} else if (computerSelection === "rock") {
+    if (playersSelection === "paper") {
+        console.log("You win! paper beats rock")
+    } else {
+        console.log("You lose! rock beats scissors")
+    }
+} else if (computerSelection === "paper") {
+    if (playersSelection === "rock") {
+        console.log("You Lose! paper beats rock")
+    } else {
+        console.log("You win! scissors beats paper")
+    }
+} else {
+    if (playersSelection === "paper") {
+        console.log("You Lose! scissors beat paper")
+    } else {
+        console.log("You win! rock beats scissors")
+    }
+}
