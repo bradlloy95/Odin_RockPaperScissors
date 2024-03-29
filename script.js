bdd9e0598ff17d486f84d0400ad7cee37d2a2cf9
bradlloy95 <136913328+bradlloy95@users.noreply.github.com>
2024-03-29 17:36:55 +0000

    final commit

--- script.js
+++ script.js
@@ -25,45 +25,62 @@ function getPlayerchoice() {
 
 
 function playRound(playersSelection, computerSelection) {
+    let result = document.createElement('h3');
     console.log(`computer = ${computerSelection}`)
     if (computerSelection === playersSelection) {
         console.log("Draw!")
+        result.textContent = "Draw!"
+        result.style.color = 'yellow'
+        resultsContainer.replaceChildren(result)
         return 0;
     } else if (computerSelection === "rock") {
         if (playersSelection === "paper") {
             console.log("You win! paper beats rock")
-            alert("You win! paper beats rock")
+            result.textContent = "You win! paper beats rock"
+            result.style.color = 'green'
+            resultsContainer.replaceChildren(result)
             return "win"
         } else {
             console.log("You lose! rock beats scissors")
-            alert("You lose! rock beats scissors")
+            result.textContent = "You lose! rock beats scissors"
+            result.style.color = 'red'
+            resultsContainer.replaceChildren(result)
             return "lose"
         }
     } else if (computerSelection === "paper") {
         if (playersSelection === "rock") {
             console.log("You Lose! paper beats rock")
-            alert("You Lose! paper beats rock")
+            result.textContent = "You Lose! paper beats rock"
+            result.style.color = 'red'
+            resultsContainer.replaceChildren(result)
             return "lose"
 
         } else {
             console.log("You win! scissors beats paper")
-            alert("You win! scissors beats paper")
+            result.textContent = "You win! scissors beats paper"
+            result.style.color = 'green'
+            resultsContainer.replaceChildren(result)
             return "win"
 
         }
     } else {
         if (playersSelection === "paper") {
             console.log("You Lose! scissors beat paper")
-            alert("You Lose! scissors beat paper")
+            result.textContent = "You Lose! scissors beat paper"
+            result.style.color = 'red'
+            resultsContainer.replaceChildren(result)
             return "lose"
 
         } else {
             console.log("You win! rock beats scissors")
-            alert("You win! rock beats scissors")
+            result.textContent = "You win! rock beats scissors"
+            result.style.color = 'green'
+            resultsContainer.replaceChildren(result)
             return "win"
 
         }
     }
+
 }
 
 
@@ -105,16 +122,22 @@ function reset() {
     roundN.textContent = round;
     PlScoreElement.textContent = playerScore;
     CMScoreElement.textContent = computerScore;
+    resultsContainer.replaceChildren();
+    buttonContainer.style.display = "";
 }
 
 let playerScore = 0;
 let computerScore = 0;
 let round = 1;
-const PlScoreElement = document.querySelector("#playerScore")
-const CMScoreElement = document.querySelector("#computerScore")
-const roundN = document.querySelector('#roundN')
+const PlScoreElement = document.querySelector("#playerScore");
+const CMScoreElement = document.querySelector("#computerScore");
+const roundN = document.querySelector('#roundN');
+const resultsContainer = document.querySelector('#results');
+const playAgain = document.createElement('button');
+const buttonContainer = document.querySelector('.buttonsContainer');
 // buttons is a node list. It looks and acts much like an array.
 const buttons = document.querySelectorAll("button");
+let gameOver = document.createElement('h1');
 
 // we use the .forEach method to iterate through each button
 buttons.forEach((button) => {
@@ -132,15 +155,39 @@ buttons.forEach((button) => {
         CMScoreElement.textContent = computerScore;
     }
 
-    if (playerScore === 2) {
-        alert("You win!!")
-        reset()
-    } else if (computerScore === 2) {
-        alert("You Lose!!")
-        reset()
+   
+    playAgain.innerHTML = "Play again"
+    playAgain.setAttribute("id", "playAgain")
+
+
+    if (playerScore === 3) {
+        gameOver.textContent = "You Win the game!";
+        gameOver.style.cssText = "font-weight: bold; color: green;"
+        buttonContainer.style.display = "none"
+        resultsContainer.appendChild(gameOver);
+        resultsContainer.appendChild(playAgain);
+
+        playAgain.addEventListener("click", () => {
+            reset();
+        });
+        
+        //reset()
+    } else if (computerScore === 3) {
+        gameOver.textContent = "You Lost the game!";
+        gameOver.style.cssText = "font-weight: bold; color: red;"
+        buttonContainer.style.display = "none"
+        resultsContainer.appendChild(gameOver);
+        resultsContainer.appendChild(playAgain);
+
+        playAgain.addEventListener("click", () => {
+            reset();
+        });
+               //reset()
     }
     round += 1;
     roundN.textContent = round; 
+
     
   });
 });
+
